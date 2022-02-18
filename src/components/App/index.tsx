85342f6677cd6f7cf2c7e011ac857a01964b3d6d
Charlie <charlie@lawsnote.com>
2022-02-18 17:59:04 +0800

    [feat] Get dateRange from DateRangePicker

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,32 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import dayjs from "dayjs";
+
 import DateRangePicker from "@Components/Common/DateRangePicker";
+import Typography from "@mui/material/Typography";
+
+import { DateRange } from "./types";
+
+const DATE_RANGE: DateRange = {
+  start: "",
+  end: "",
+};
 
 function App(): React.ReactElement {
+  const [dateRange, setDateRange] = useState(DATE_RANGE);
+
   return (
     <main>
-      <DateRangePicker />
+      <DateRangePicker setDateRange={setDateRange} />
+      <Typography>
+        起始日期：
+        {dateRange.start.length > 0 &&
+          dayjs(dateRange.start).format("YYYY年MM月DD日")}
+      </Typography>
+      <Typography>
+        結束日期：
+        {dateRange.end.length > 0 &&
+          dayjs(dateRange.end).format("YYYY年MM月DD日")}
+      </Typography>
     </main>
   );
 }
